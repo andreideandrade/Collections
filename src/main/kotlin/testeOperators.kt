20dc12cd447cecefd7173e2283ad1838344c0700
ANDREI DE ANDRADE <eng.andreilino@gmail.com>
2021-07-22 18:05:53 -0300

    Created Int Array Of Estudando matriz basica Arrays de Double Operações count, find e any

--- src/main/kotlin/testeOperators.kt
+++ src/main/kotlin/testeOperators.kt
@@ -6,15 +6,21 @@ fun main() {
 
     for (salario in salarios){  println(salario) }
 
-    println("--------------------------------")
-
+    println("----------- max - min - average -------------")
     println("Maior salario: ${salarios.maxOrNull()}")
     println("Menor salario: ${salarios.minOrNull()}")
     println("Média salarial: ${salarios.average()}")
 
-    println("--------------------------------")
-
+    println("------------ filter --------------------")
     val salarioMaiorQue2500 = salarios.filter { it > 2500.0 }
     salarioMaiorQue2500.forEach { println(it) }
 
+    println("------------ count --------------------")
+    println(salarios.count{ it in 2000.0..5000.0})
+
+    println("------------ find --------------------")
+    println(salarios.find{ it == 2250.0})
+
+    println("------------ any --------------------")
+    println(salarios.any{ it == 2250.0})
 }
