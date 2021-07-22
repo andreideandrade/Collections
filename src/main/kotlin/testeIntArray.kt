8425acb514c68951ba6041e6cf67c1ce118efee0
ANDREI DE ANDRADE <eng.andreilino@gmail.com>
2021-07-21 22:32:40 -0300

    Creatd Int Array Class Estudando matriz basica Adicionado comentários

--- src/main/kotlin/testeIntArray.kt
+++ src/main/kotlin/testeIntArray.kt
@@ -4,23 +4,28 @@ fun main() {
 
     val values = IntArray(5)
 
+    /* Popula matriz    */
     values[0] = 9
     values[1] = 1
     values[2] = 6
     values[3] = 2
     values[4] = 7
 
+    /*  imprime usando variaveis    */
     println("for usando variaveis: ")
     for (valor in values){  print(" ${valor} ")   }
 
     values.sort()   // Organiza matriz em ordem crescente
 
+    /*  Como usar o forEach     */
     println("\nforEach (Usa função lambda 'it') com sort: ")
     values.forEach {  print(" ${it} ")    }
 
     println("\nforEach (Usando variavel 'valor') com sort: ")
     values.forEach {  valor -> print(" ${valor} ")    }
 
+    /*  Manipulando matrises usando index   */
     println("\nfor usando index: ")
     for (index in values.indices){ print(" ${values[index]} ") }
+
 }
