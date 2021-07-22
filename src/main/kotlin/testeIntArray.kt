848afd4e33a14cf2185e5f108faaad37d82bffe9
ANDREI DE ANDRADE <eng.andreilino@gmail.com>
2021-07-21 22:02:24 -0300

    Creatd Int Array Class Estudando matriz basica

---
+++ src/main/kotlin/testeIntArray.kt
@@ -0,0 +1,28 @@
+package me.andrei.collections
+
+fun main() {
+
+    val values = IntArray(5)
+
+    values[0] = 9
+    values[1] = 1
+    values[2] = 6
+    values[3] = 2
+    values[4] = 7
+
+
+    println("for usando variaveis: ")
+    for (valor in values){  print(" ${valor} ")   }
+
+    values.sort()
+
+    println("\nforEach (Usa função lambda 'it') com sort: ")
+    values.forEach {  print(" ${it} ")    }
+
+    println("\nforEach (Usando variavel 'valor') com sort: ")
+    values.forEach {  valor -> print(" ${valor} ")    }
+
+    println("\nfor usando index: ")
+    for (index in values.indices){ print(" ${values[index]} ") }
+
+}
