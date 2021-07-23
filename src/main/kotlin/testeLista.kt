bdf149f69a2a421ddb4819a9e2c2dbe6de325141
ANDREI DE ANDRADE <eng.andreilino@gmail.com>
2021-07-22 21:51:14 -0300

    Estudando sortedBy

--- src/main/kotlin/testeLista.kt
+++ src/main/kotlin/testeLista.kt
@@ -1,9 +1,9 @@
 package me.andrei.collections
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val nailha = Funcionario("Nailha", 2000.0)
-    val maria = Funcionario("Maria", 3000.0)
+    val joao = Funcionario("João", 2000.0)
+    val nailha = Funcionario("Nailha", 3100.0)
+    val maria = Funcionario("Maria", 1000.0)
 
     val funcionarios = listOf(joao, nailha, maria)
 
@@ -11,6 +11,12 @@ fun main() {
 
     println("--------------------------")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("-------- sortedBy ---------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
 }
 
 data class Funcionario(
